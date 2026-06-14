fff2214a1db42c14aadb93d7de7d0f5458c8dfb2
Dmitry Dementyev-Dedelis <dmitry@affire.ai>
2026-06-14 18:15:34 +0300

    ver bump to 1.0.0

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     title="Tesla Nord Pool",
-    version="0.1.0",
+    version="1.0.0",
     lifespan=lifespan,
     docs_url="/docs",
     redoc_url=None,

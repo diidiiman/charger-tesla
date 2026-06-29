d5efcfb736e7d2fd483b32093611bef65342be67
D3 <dmitry@clankersystems.com>
2026-06-29 21:07:42 +0300

    date picker version bump

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,8 @@
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
